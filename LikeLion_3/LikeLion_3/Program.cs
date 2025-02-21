f4f6fc32adcf9d849d4438e09df2b53dcadc6bb2
eogk31 <134608027+eogk31@users.noreply.github.com>
2025-02-21 10:42:30 +0900

    Study20250221_3

---
+++ LikeLion_3/LikeLion_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //캐릭터
+            //hp: 100
+            //att: 56.7
+            //캐릭터이름: ??
+            //등급: S
+
+            int hp = 100;
+            double att = 56.7;
+            string name = "배홍범";
+            char rank = 'S';
+
+            Console.WriteLine("캐릭터 프로필");
+            Console.WriteLine("------------------");
+            Console.WriteLine($"hp: {hp}");
+            Console.WriteLine($"att: {att}");
+            Console.WriteLine($"캐릭터이름: {name}");
+            Console.WriteLine($"등급: {rank}");
+
+        }
+    }
+}
