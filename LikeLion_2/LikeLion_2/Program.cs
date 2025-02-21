010f7b2350729f30a40d207bfc586353303662f6
eogk31 <134608027+eogk31@users.noreply.github.com>
2025-02-21 10:22:50 +0900

    Study20250221_2

---
+++ LikeLion_2/LikeLion_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언
+            int age = 25; //정수형 변수 선언과 값 저장.
+            Console.WriteLine(age); //변수에 저장된 값 출력.(25)
+        }
+    }
+}
